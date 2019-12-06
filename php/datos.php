928d591b04f8104d76a52b2975672df0aeb9c7fc
agus <agu.99@hotmail.com>
2019-12-05 21:09:25 -0300

    fotos

---
+++ php/datos.php
@@ -0,0 +1,12 @@
+<<?php 
+
+$name = $_POST ['name'];
+$mail = $_POST['mail'];
+$consulta = $_POST ['consulta'];
+
+	ECHO "<h2> INFORMACION RECIBIDA </h2>"
+	ECHO "El nombre recibido es". $name."<br/>";
+	ECHO "El mail recibido es" . $mail."<br/>";
+	ECHO "La consulta es" . $consulta . "<br/>";
+
+ ?>
